e2fce6324820a662d02dea40ac865b957b344fc3
Dovias <dovias@zohomail.eu>
2022-02-14 22:13:49 +0200

    Added methods for binary ItemStack (de)serialization.

--- src/main/java/lt/wayout/minecraft/plugin/wayengine/util/Serialization.java
+++ src/main/java/lt/wayout/minecraft/plugin/wayengine/util/Serialization.java
@@ -7,9 +7,15 @@ import org.bukkit.craftbukkit.v1_18_R1.inventory.CraftItemStack;
 import org.bukkit.craftbukkit.v1_18_R1.util.CraftMagicNumbers;
 import org.bukkit.inventory.ItemStack;
 
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.*;
 
 public class Serialization {
@@ -17,52 +23,65 @@ public class Serialization {
     private Serialization() {}
 
     @NotNull
-    public static List<Map<String, Object>> serialize(@NotNull final List<ItemStack> itemStacks, boolean dataTypes) {
-        List<Map<String, Object>> data = new ArrayList<>(itemStacks.size());
-        for (ItemStack itemStack : itemStacks) {
-            if (itemStack == null) continue;
-            data.add(Serialization.serialize(itemStack, dataTypes));
-        }
-        return data;
-    }
-
-    @NotNull
-    public static List<Map<String, Object>> serialize(@NotNull final ItemStack[] itemStacks, boolean dataTypes) {
+    public static List<Map<String, Object>> serializeItems(final ItemStack @NotNull [] itemStacks, boolean dataTypes) {
         List<Map<String, Object>> data = new ArrayList<>(itemStacks.length);
         for (ItemStack itemStack : itemStacks) {
-            data.add(Serialization.serialize(itemStack, dataTypes));
+            if (itemStack == null) continue;
+            data.add(Serialization.serializeItem(itemStack, dataTypes));
         }
         return data;
     }
 
     @NotNull
-    public static Map<String, Object> serialize(@NotNull final ItemStack itemStack, boolean dataTypes) {
+    public static Map<String, Object> serializeItem(@NotNull final ItemStack itemStack, boolean dataTypes) {
         Map<String, Object> data = new LinkedHashMap<>();
         data.put("type", itemStack.getType().getKey().asString());
         data.put("amount", itemStack.getAmount());
         CompoundTag compoundTag = CraftItemStack.asNMSCopy(itemStack).getTag();
         if (compoundTag == null) return data;
-        Object serializedData = Serialization.serialize(compoundTag, dataTypes);
+        Object serializedData = Serialization.serializeTag(compoundTag, dataTypes);
         if (!(serializedData instanceof Map<?, ?> mapData) || mapData.isEmpty()) return data;
         data.put("data", mapData);
         return data;
     }
 
+    public static byte @Nullable [] serializeItem(@NotNull final ItemStack itemStack) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try (BukkitObjectOutputStream bukkitOutputStream = new BukkitObjectOutputStream(outputStream)) {
+            bukkitOutputStream.writeObject(itemStack);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            return null;
+        }
+        return outputStream.toByteArray();
+    }
+
+    public static byte @Nullable [] serializeItems(final ItemStack @NotNull [] itemStacks) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try (BukkitObjectOutputStream bukkitOutputStream = new BukkitObjectOutputStream(outputStream)) {
+            bukkitOutputStream.writeObject(itemStacks);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            return null;
+        }
+        return outputStream.toByteArray();
+    }
+
     // Ugly. Needs something more fancy and cleaner looking.
     @Nullable
-    public static Object serialize(@NotNull final Tag tag, boolean dataTypes) {
+    public static Object serializeTag(@NotNull final Tag tag, final boolean dataTypes) {
         // VERY UGLY! I CAN'T STAND LOOKING AT THIS.
         if (tag instanceof CompoundTag cTag) {
             Map<String, Object> dataMap = new HashMap<>();
             for (Map.Entry<String, Tag> entry : cTag.tags.entrySet()) {
-                dataMap.put(entry.getKey(), Serialization.serialize(entry.getValue(), dataTypes));
+                dataMap.put(entry.getKey(), Serialization.serializeTag(entry.getValue(), dataTypes));
             }
             return dataMap;
         } else if (tag instanceof CollectionTag<?> cTag) {
             List<Object> list = new ArrayList<>(cTag.size());
             for (int i = 0; i < cTag.size(); i++) {
                 Tag elementTag = cTag.get(i);
-                list.add(Serialization.serialize(elementTag, false));
+                list.add(Serialization.serializeTag(elementTag, false));
             }
             if (dataTypes && !(cTag instanceof ListTag)) {
                 Map<String, Object> map = new LinkedHashMap<>();
@@ -86,28 +105,50 @@ public class Serialization {
     }
 
     @NotNull
-    public static List<ItemStack> deserialize(@NotNull final List<Map<?, ?>> serializedData) {
+    public static List<ItemStack> deserializeItems(@NotNull final List<Map<?, ?>> serializedData) {
         List<ItemStack> items = new ArrayList<>();
         for (Map<?, ?> serializedMap : serializedData) {
-            ItemStack serializedStack = Serialization.deserialize(serializedMap);
+            ItemStack serializedStack = Serialization.deserializeItem(serializedMap);
             if (serializedStack != null) items.add(serializedStack);
         }
         return items;
     }
 
     @Nullable
-    public static ItemStack deserialize(@NotNull final Map<?, ?> serializedData) {
+    public static ItemStack[] deserializeItems(byte[] serializedData) {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(serializedData);
+        try (BukkitObjectInputStream bukkitInputStream = new BukkitObjectInputStream(inputStream)) {
+            return bukkitInputStream.readObject() instanceof ItemStack[] itemStacks ? itemStacks : null;
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Nullable
+    public static ItemStack deserializeItem(byte[] serializedData) {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(serializedData);
+        try (BukkitObjectInputStream bukkitInputStream = new BukkitObjectInputStream(inputStream)) {
+            return bukkitInputStream.readObject() instanceof ItemStack itemStack ? itemStack : null;
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Nullable
+    public static ItemStack deserializeItem(@NotNull final Map<?, ?> serializedData) {
         if (!(serializedData.get("type") instanceof String type) || !(serializedData.get("amount") instanceof Integer amount)) return null;
         Material material = Material.matchMaterial(type);
         if (material == null) return null;
         net.minecraft.world.item.ItemStack nmsItemStack = new net.minecraft.world.item.ItemStack(CraftMagicNumbers.getItem(material), amount);
-        if (!(serializedData.get("data") instanceof Map<?, ?> serializedNbtMap) || !(Serialization.deserialize((Object)serializedNbtMap) instanceof CompoundTag compoundTag)) return CraftItemStack.asBukkitCopy(nmsItemStack);
+        if (!(serializedData.get("data") instanceof Map<?, ?> serializedNbtMap) || !(Serialization.deserializeTag(serializedNbtMap) instanceof CompoundTag compoundTag)) return CraftItemStack.asBukkitCopy(nmsItemStack);
         nmsItemStack.setTag(compoundTag);
         return CraftItemStack.asBukkitCopy(nmsItemStack);
     }
 
     @Nullable
-    public static Tag deserialize(@NotNull final Object serializedData) {
+    public static Tag deserializeTag(@NotNull final Object serializedData) {
         if (serializedData instanceof Map<?, ?> dataMap) {
             if (dataMap.get("type") instanceof String key) {
                 Object value = dataMap.get("value");
@@ -131,7 +172,7 @@ public class Serialization {
                     } else if (value instanceof List<?> list) {
                         // Kinda breaks DRY principle.
                         if (key.equalsIgnoreCase("list")) {
-                            return Serialization.deserialize(serializedData);
+                            return Serialization.deserializeTag(serializedData);
                         } else if (key.equalsIgnoreCase("long_array")) {
                             long[] array = new long[list.size()];
                             if (list.get(0) instanceof Number) {
@@ -164,7 +205,7 @@ public class Serialization {
 
             for (Map.Entry<?, ?> entry : dataMap.entrySet()) {
                 if (!(entry.getKey() instanceof String key)) continue;
-                Tag tag = Serialization.deserialize(entry.getValue());
+                Tag tag = Serialization.deserializeTag(entry.getValue());
                 if (tag == null) continue;
                 compoundTag.put(key, tag);
             }
@@ -173,7 +214,7 @@ public class Serialization {
         else if (serializedData instanceof List<?> listData) {
             ListTag listTag = new ListTag();
             for (int i = 0; i < listData.size(); i++) {
-                Tag tag = Serialization.deserialize(listData.get(i));
+                Tag tag = Serialization.deserializeTag(listData.get(i));
                 if (tag == null) continue;
                 listTag.add(i, tag);
             }
